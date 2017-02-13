1a44d3a0e411a8aea36ef124fdbdf375e6c7a082
Jakub <kubawl.91@gmail.com>
2017-02-13 13:28:56 +0100

    add menu_handle to main

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
+from ui import Ui
+from cities import Cities
+import sys
+import os
+
 
 class Menu:
+    """
+    handle full menu options
+    """
 
     @staticmethod
     def menu_handle():
-        pass
+        while True:
+            Ui.print_menu()
+            option = Ui.get_input("Choose an option: ")
+            if option == '1':
+                pass
+            elif option == '2':
+                pass
+            elif option == '3':
+                pass
+            elif option == '4':
+                pass
+            elif option == '5':
+                pass
+            elif option == '0':
+                os.system('clear')
+                sys.exit()
+                pass
 
 
 def main():
+    Cities.create_table_csv()
     Menu.menu_handle()
 
 if __name__ == '__main__':
