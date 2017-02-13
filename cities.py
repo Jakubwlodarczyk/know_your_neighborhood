2f624343d84c5a29cf6d81b89e24ef0fab1d055c
Jakub <kubawl.91@gmail.com>
2017-02-13 13:15:29 +0100

    add methods: create_table_csv, read_file

--- cities.py
+++ cities.py
@@ -1,6 +1,8 @@
+import csv
 
 
 class Cities:
+
     city_list = []
 
     def __init__(self, province, county, community, rgmi, name, type):
@@ -13,3 +15,27 @@ class Cities:
 
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def create_table_csv():
+        """
+        Create object and append to list
+        """
+        city_table = Cities.read_file('malopolska.csv')
+        for row in city_table:
+            new_object = Cities(row[0], row[1], row[2], row[3], row[4], row[5])
+            Cities.city_list.append(new_object)
+
+    @staticmethod
+    def read_file(file_name):
+        """
+        Read csv file
+        :param file_name: name of file to read
+        :return: list
+        """
+        new_table = []
+        with open(file_name, 'r') as f:
+            reader = csv.reader(f, delimiter='\t')
+            for row in reader:
+                new_table.append(row)
+        return new_table[1:]
