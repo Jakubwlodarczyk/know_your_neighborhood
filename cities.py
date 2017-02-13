269d7a520f4fa7b81a0361fba04688b72cf727cc
Jakub <kubawl.91@gmail.com>
2017-02-13 14:23:21 +0100

    add : get_name_list , get_longest_name

--- cities.py
+++ cities.py
@@ -65,3 +65,77 @@ class Cities:
             if object_to_check.type == item[0]:
                 item[1] += 1
         return statistic_list
+
+    @classmethod
+    def get_name_list(cls):
+        """
+        :return: Name of all cities without duplicate
+        """
+        name_list = []
+        for item in cls.city_list:
+            if item.name not in name_list:
+                name_list.append(item.name)
+        return name_list
+
+    @staticmethod
+    def get_longest_words(words_list, amount=3):
+        """
+        Check longest word in list
+        :param words_list: list of words to check
+        :param amount: how many words should be returned
+        :return: list of words
+        """
+        words_list.sort(key=len, reverse=True)
+        return words_list[:amount]
+
+
+    @classmethod
+    def get_largest_county(cls):
+        """
+        Check which count is the largest
+        :return: The largest county
+        """
+        county_number = ''
+        community_number = 0
+        for item in Cities.city_list:
+            if item.community:
+                if int(item.community) > community_number:
+                    community_number = int(item.community)
+                    county_number = item.county
+        for item in Cities.city_list:
+            if county_number == item.county and item.type == 'powiat':
+                return item
+
+    @classmethod
+    def get_multi_types_objects(cls):
+        """
+        Check which object has more than 1 type
+        :return: list ( contain object with more than 1 type)
+        """
+        cities = []
+        multi_types = []
+        for item in Cities.city_list:
+            if item.name in cities:
+                if item.name not in multi_types:
+                    multi_types.append(item.name)
+            else:
+                cities.append(item.name)
+        return multi_types
+
+    @classmethod
+    def search(cls, user_input):
+        """
+        Search matching words
+        :param user_input: fragment or whole word to search
+        :return: list with matching words
+        """
+        search_result = []
+        for item in Cities.city_list:
+            if user_input in item.name:
+                row = [item.name, item.type]
+                search_result.append(row)
+        if search_result:
+            return search_result
+        else:
+            return False
+
